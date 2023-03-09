b4e208d87423f09d7e078d0a2a43354f47c76278
Ashkaun Iranfar <ashkauniranfar@gmail.com>
2023-03-09 12:53:24 -0800

    phase 1 completed

--- frontend/src/components/PokemonBrowser.js
+++ frontend/src/components/PokemonBrowser.js
@@ -1,6 +1,7 @@
-import { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDebugValue, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { NavLink, Route, useParams } from 'react-router-dom';
+import { getPokemon } from '../store/pokemon';
 
 import PokemonDetail from './PokemonDetail';
 import CreatePokemonForm from './CreatePokemonForm';
@@ -13,6 +14,11 @@ const PokemonBrowser = () => {
   });
   const [showForm, setShowForm] = useState(false);
 
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPokemon())
+  })
 
   if (!pokemon) {
     return null;
